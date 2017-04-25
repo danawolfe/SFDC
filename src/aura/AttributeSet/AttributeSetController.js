f171cc887463361f61946213c0f75329a64bc57b
Dana Wolfe <git44147@outlook.com>
2017-04-25 18:34:27 -0400

    expand /collapse working, before trying layout component

--- src/aura/AttributeSet/AttributeSetController.js
+++ src/aura/AttributeSet/AttributeSetController.js
@@ -1,5 +1,29 @@
 ({
 	myAction : function(component, event, helper) {
 		
-	}
+	},  
+	
+	doInit: function(component, event, helper) {      
+    // Fetch the account list from the Apex controller   
+    console.log("In doInit()");
+    
+    helper.getAttributeSetItems(component);
+	},
+  	
+  	handleToggleExpand: function(component, event, helper){
+	  console.log('Clicked on Chevron');
+	  var target = event.getSource();
+//	  console.log('target:' + target.get("v.iconName"));
+	  if (target.get("v.iconName") == "utility:chevronright"){
+		  target.set("v.iconName","utility:chevrondown");
+	  }
+	  else
+	  {
+		  target.set("v.iconName","utility:chevronright");
+	  }
+	  var listOfAttributes = component.find("attrRow");
+	  $A.util.toggleClass(listOfAttributes, "toggle");
+	  //target.set("v.iconName","utility:chevrondown")
+	  //console.log('iconName:' + target.toString() );
+  }
 })
--- src/aura/AttributeSet/AttributeSetHelper.js
+++ src/aura/AttributeSet/AttributeSetHelper.js
@@ -1,5 +1,16 @@
 ({
-	helperMethod : function() {
-		
-	}
+  // Fetch the accounts from the Apex controller
+  getAttributeSetItems: function(component) {
+    var action = component.get('c.getAttributeSetItems');
+    // Get the Attribute Set ID and pass to the server side method
+    var attributeSet = component.get('v.attributeSet');
+    action.setParams({ setId : attributeSet.Id });
+    
+    // Set up the callback
+    var self = this;
+    action.setCallback(this, function(actionResult) {
+     component.set('v.attributes', actionResult.getReturnValue());
+    });
+    $A.enqueueAction(action);
+  }
 })
